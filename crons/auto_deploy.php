13e773e6304c29a7ed17e6969f1dcd4049afecda
Thiago Hofmeister <thiago.hofmeister@gmail.com>
2017-08-02 20:47:15 -0300

    Atualizar script de pull automatico

--- crons/auto_deploy.php
+++ crons/auto_deploy.php
@@ -3,15 +3,21 @@
 $token = $_GET['token'];
 
 if ($token == 'a5f55ae5bd9674dd11b680666248acd1') {
-	$exec = shell_exec("git pull origin master 2>&1");
-	echo $exec;
+    shell_exec("rm -rf ../.git/refs/remotes/origin");
+    shell_exec("rm -rf ../.git/logs/refs/heads/master/HEAD");
+    shell_exec("rm -rf ../.git/logs/refs/heads/master");
+    shell_exec("rm -rf ../.git/logs/HEAD");
 
-	$textoLog = PHP_EOL."Data: ".date(d."/".m."/".Y." - ".H.":".i.":".s);
-	$textoLog .= PHP_EOL.$exec;
 
-	$arquivoLog = fopen('log.txt', 'a+');
-	fwrite($arquivoLog, $textoLog);
-	fclose($arquivoLog);
+    $exec = shell_exec("git pull origin master 2>&1");
+    echo $exec;
+
+    $textoLog = PHP_EOL."Data: ".date(d."/".m."/".Y." - ".H.":".i.":".s);
+    $textoLog .= PHP_EOL.$exec;
+
+    $arquivoLog = fopen('log.txt', 'a+');
+    fwrite($arquivoLog, $textoLog);
+    fclose($arquivoLog);
 }
 ?>
 </pre>
