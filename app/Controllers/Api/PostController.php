8d81dc8b52abdd19fb54c6b8e0b319dbec310398
Thiago Hofmeister <thiago.hofmeister@gmail.com>
2017-12-02 17:48:27 -0200

    Corrigir herança da controller

--- app/Controllers/Api/PostController.php
+++ app/Controllers/Api/PostController.php
@@ -2,13 +2,25 @@
 
 namespace App\Controllers\Api;
 
+use App\Controllers\Controller;
+use App\Models\Post as Model;
+
 /**
  * @todo Document class PostController.
  *
  * @author Thiago Hofmeister <thiago.souza@moovin.com.br>
  */
-class PostController
+class PostController extends Controller
 {
+    /** @var Model */
+    private $Post;
+
+    public function __construct(Model $post = null) {
+        parent::__construct();
+
+        $this->Post = $post;
+    }
+
     public function getPosts()
     {
         return response()->json($this->Post->getPosts());
